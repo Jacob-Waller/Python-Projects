54709b4f4f647df072d71b71890cfd7a61fc341b
Jacob_Waller <38044292+Jacob-Waller@users.noreply.github.com>
2022-08-03 19:09:37 -0400

    Added color to the background screen and created a new file where all the settigns will be at.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import sys
 
-Import pygam
+import pygame
 
 def run_game():
   #This will initalize the game and create a screen object
   pygame.init()
   screen = pygame.display.set_mode((1200, 800))
   pygame.display.set_caption("Alien Invasion")
+
+  # Set the background color
+  bg_color = (230, 230, 230)
   
   #Start of the main loop for the game
   while True:
   
     #Watches for keyboard and mouse events
     for event in pygame.event.get():
+
       if event.type == pygame.QUIT:
         sys.exit()
+
+      screen.fill(bg_color)
         
     # Make the most recently drawn screen visible
     pygame.display.flip()
