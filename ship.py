f681bff50ef2356a8bc1deaa5025c40729fa686e
Jacob_Waller <38044292+Jacob-Waller@users.noreply.github.com>
2022-08-03 21:43:43 -0400

    Changed speed for leftwards movement

--- ship.py
+++ ship.py
@@ -21,7 +21,10 @@ class Ship():
     def update(self):
         """Update the ship's position based on the movement flag"""
         if self.moving_right:
-                self.rect.centerx += 1
+            self.rect.centerx += 1
+            
+        elif self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         """Draw the ship at its current location"""
