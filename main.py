c422b3d41e35e63950e482ec914b9dab26c2de02
Jacob_Waller <38044292+Jacob-Waller@users.noreply.github.com>
2022-08-03 20:50:00 -0400

    Added the ship to the main screen of the game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
 
@@ -15,6 +16,8 @@ def run_game():
 
   pygame.display.set_caption("Alien Invasion")
 
+  # Make a shnip
+  ship = Ship(screen)
 
   # Set the background color
   bg_color = (230, 230, 230)
@@ -29,6 +32,7 @@ def run_game():
         sys.exit()
 
       screen.fill(ai_settings.bg_color)
+      ship.blitme()
         
     # Make the most recently drawn screen visible
     pygame.display.flip()
