dc5e7b6fdecfb98e259bf2d301649b79274907fd
Jacob_Waller <38044292+Jacob-Waller@users.noreply.github.com>
2022-08-09 00:06:49 -0400

    Created the bullet class to be able to shoot

--- bullets.py
+++ bullets.py
@@ -6,7 +6,7 @@ class Bullet(Sprite):
 
     def __init__(self, ai_settings, screen, ship):
         """Create a bulet object at the ship's current position"""
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
         # Create a bullet rect at (0, 0) and then set correct position
@@ -19,3 +19,15 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet
+        self.y -= self.speed.factor
+
+        # Update the rect position
+        self.rect.y = self.y
+    
+    def draw_bullet(self):
+        """Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
